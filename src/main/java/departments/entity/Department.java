2b2727ec49613099d51c3ef9f6f13e138e192eb1
Kaka180302 <183anhka@gmail.com>
2026-07-22 12:26:19 +0700

    update entity ddeparrtment

--- src/main/java/departments/entity/Department.java
+++ src/main/java/departments/entity/Department.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -40,6 +42,13 @@ public class Department {
 
     @OneToMany(mappedBy = "department")
     private List<Appointment> appointments;
+    
+    @ManyToOne
+    @JoinColumn(name = "parent_id") 
+    private Department parent;
+
+    @OneToMany(mappedBy = "parent") 
+    private List<Department> subDepartments;
     
 
     public BigDecimal getMinFee() {
